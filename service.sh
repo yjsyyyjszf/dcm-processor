028527018d54abf869694c873aaa2e9c945380ad
Giles Ekow Tetteh <giles.tetteh@tum.de>
2021-04-16 20:45:50 +0200

    added service management script

--- service.sh
+++ service.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
 BASEDIR=./mapped_folders
+
+if [ $1 = "install" ]
+then
+  
+  while getopts n:p: flag
+  do
+    case "${flag}" in
+        n) servicename=${OPTARG};;
+        p) servicepath=${OPTARG};;
+    esac
+  done
+
+  if [ -z "$servicename" ]
+  then
+    read -p "Enter Service Name :" servicename
+  fi
+
+  if [ -z "$servicepath" ]
+  then
+    read -p "Enter Service Path :" servicepath
+  fi
+  
+  cp -r "$servicepath/module" "$BASEDIR/modules/$servicename"
+  cp -r "$servicepath/registry" "$BASEDIR/registry/$servicename"
+
+fi

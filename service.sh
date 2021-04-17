2cc807ed849f8bcf2bb1bfa7080be0df346b2e4a
Giles Ekow Tetteh <giles.tetteh@tum.de>
2021-04-17 17:07:57 +0200

    updated service.sh to check env vars

--- service.sh
+++ service.sh
@@ -35,6 +35,9 @@ valid_action_type()
     esac
 }
 
+[ -z "$BASEDIR" ] && echo "set BASEDIR variable in the .env file" && exit 1
+[ -z "$MODULES" ] && echo "set MODULES variable in the .env file" && exit 1
+[ -z "$REGISTRY" ] && echo "set REGISTRY variable in the .env file" && exit 1
 
 unset ACTION SERVICENAME SERVICEPATH BACKUPPATH
 

ec3d59419f9bdcbcbdd409c6d3cf1ebefcfb3fa4
Giles Ekow Tetteh <giles.tetteh@tum.de>
2021-04-17 16:50:31 +0200

    changed worker container to read requirements and scripts from service folders

--- containers/worker/entrypoint.sh
+++ containers/worker/entrypoint.sh
@@ -5,23 +5,25 @@ export DEBIAN_FRONTEND=noninteractive
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-REQUIREMENTS="$MODULES/requirements.txt"
+REQUIREMENTS="$MODULES/*/requirements.txt"
 SLOGS="$LOGS/shell.txt"
 PLOGS="$LOGS/pip.txt"
-SCRIPT="$MODULES/script.sh"
+SCRIPTS="$MODULES/*/script.sh"
 
 alias pip='python -m pip'
 
-if [ -f "$REQUIREMENTS" ]; then
-    python -m pip install -r "$REQUIREMENTS"
-fi
+for REQUIREMENT in $(ls $REQUIREMENTS)
+do
+  python -m pip install -r "$REQUIREMENT"
+done
 
-if [ -f "$SCRIPT" ]; then
-    bash "$SCRIPT"
-fi
+for SCRIPT in $(ls $SCRIPTS)
+do
+  bash "$SCRIPT"
+done
 
 when-changed "$REQUIREMENTS" -c python -m pip install -r %f >> $PLOGS &
 
-when-changed "$SCRIPT" -c bash %f >> $SLOGS &
+when-changed "$SCRIPTS" -c bash %f >> $SLOGS &
 
 rq worker --path "$MODULES" -u "redis://:$REDIS_PSWD@$REDIS_HOST:$REDIS_PORT"  $JOBS
